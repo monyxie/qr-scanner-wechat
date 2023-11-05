bcafa74cd112459eb1aa978e020fcf0c4e7e09a0
Mony Xie <monyxie@gmail.com>
2023-11-05 10:29:20 +0800

    Remove debug code

--- src/assets/wasm-loader.js
+++ src/assets/wasm-loader.js
@@ -1,7 +1,4 @@
 export async function loadWasm() {
-  console.log('loading wasm')
   const res = await fetch('/assets/opencv.wasm')
-  const buf = await res.arrayBuffer()
-  console.log(res, buf)
-  return buf
+  return await res.arrayBuffer()
 }
